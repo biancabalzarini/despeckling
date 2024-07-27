f7f800e539bdba5a3dd6ad4dd6d072832ea99a9c
Bianca Balzarini <50087855+biancabalzarini@users.noreply.github.com>
2024-07-27 12:49:33 -0300

    primera version de la funcion rGI0 en python

--- scripts/GenrationGI0.py
+++ scripts/GenrationGI0.py
@@ -1,1 +1,13 @@
-# inicializo
+### Sampling from the GI0 distribution
+# n: cantidad de muestras
+# p_alpha: el valor del parámetro alpha
+# p_gamma: el valor del parámetro gamma
+# p_Looks: la cantidad de Looks
+
+def rGI0(
+  n,
+  p_alpha,
+  p_gamma,
+  p_Looks
+):
+  return np.random.gamma(p_Looks, 1/p_Looks, n) / np.random.gamma(-p_alpha, 1/p_gamma, n)
