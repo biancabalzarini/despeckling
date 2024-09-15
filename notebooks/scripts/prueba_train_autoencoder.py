1b0aff4b7db8272cda298b3d01db759d501e554f
biancabalzarini <balzarini.bianca@gmail.com>
2024-09-15 20:34:57 -0300

    importo GI0 de la carpeta scripts

---
+++ notebooks/scripts/prueba_train_autoencoder.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import sys
+sys.path.append('..')
+from scripts.GenrationGI0 import rGI0
+
+
+# In[6]:
+
+
+rGI0(n=10, p_alpha=-1.5, p_gamma=1, p_Looks=1)
+
