ebe0856e1b07dc3409030a29b2877fb287a6f626
biancabalzarini <balzarini.bianca@gmail.com>
2024-09-23 15:41:37 -0300

    detalles

--- scripts/autoencoders.py
+++ scripts/autoencoders.py
@@ -21,7 +21,7 @@ class InMemoryImageDataset(Dataset):
 
 def generate_multiple_images(
     n: int,
-    partitioned_gi0_image: function
+    partitioned_gi0_image
 ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
     """
     Genera múltiples conjuntos de imágenes utilizando la función partitioned_gi0_image.
@@ -50,7 +50,7 @@ def generate_multiple_images(
     p_looks: List[int] = [1, 1, 1, 1]
     
     alpha_values = [-1.5, -2, -3, -5, -6, -8, -10, -20]
-
+    
     for i in range(n):
         p_alphas: List[float] = random.choices(alpha_values, k=4)
         
@@ -59,5 +59,5 @@ def generate_multiple_images(
         conjunto_g[i] = imagen_g
         conjunto_gi[i] = imagen_gi
         conjunto_gI0[i] = imagen_gI0
-
+    
     return conjunto_g, conjunto_gi, conjunto_gI0
