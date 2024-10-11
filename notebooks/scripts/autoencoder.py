3be1224960bcbfc757946cd7ac956a197a50a1ad
biancabalzarini <balzarini.bianca@gmail.com>
2024-10-11 14:41:02 -0300

    empiezo a poblar la notebook ppal

--- notebooks/scripts/autoencoder.py
+++ notebooks/scripts/autoencoder.py
@@ -18,3 +18,53 @@ from torch.utils.data import DataLoader
 import torch.nn as nn
 import torch.optim as optim
 
+
+# ---
+# # Creación del dataset para entrenar
+
+# In[ ]:
+
+
+## PARÁMETROS
+
+# Cantidad de imágenes a generar
+n = 50000
+# Tamaños de los batches
+batch_size = 50
+
+
+# In[ ]:
+
+
+train_g, train_gi, train_gI0 = generate_multiple_images(n, partitioned_gi0_image)
+
+
+# In[ ]:
+
+
+normalize_to_01 = transforms.Lambda(lambda x: (x - x.min()) / (x.max() - x.min()))
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    normalize_to_01
+])
+
+dataset_train = InMemoryImageDataset(train_gI0, train_gi, transform=transform)
+train_loader = DataLoader(dataset_train, batch_size=batch_size, shuffle=True)
+
+
+# ---
+# # Entrenamiento
+
+# In[ ]:
+
+
+# PARÁMETROS
+
+# Dimensión de la capa más interna del autoencoder
+encoding_dim = 32
+# Learning rate
+learning_rate = 1e-3
+# Cantidad de épocas
+num_epochs = 100
+
