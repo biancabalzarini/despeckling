76f5aa1bed9292a9f35284ac93da98af325eb7c3
biancabalzarini <balzarini.bianca@gmail.com>
2025-03-31 19:38:10 -0300

    importo config, modelo ya entrenado, y creo dataset de testeo para filter quality

--- notebooks/scripts/quality_filters.py
+++ notebooks/scripts/quality_filters.py
@@ -1,8 +1,69 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[1]:
 
 
+import sys
+sys.path.append('..')
 
+from scripts.GenrationGI0 import partitioned_gi0_image, generate_multiple_images, mixed_dataset
+from scripts.autoencoders import InMemoryImageDataset, ConfigurableAutoencoder
+
+import pandas as pd
+import torch
+from torchvision import transforms
+from torch.utils.data import DataLoader
+import torch.nn as nn
+import torch.optim as optim
+from torchsummary import summary
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+from omegaconf import OmegaConf
+OmegaConf.register_new_resolver("eval", eval)
+
+
+# Elegir el archivo de configuración correspondiente:
+
+# In[2]:
+
+
+config_name = 'config_1' # Elegir
+
+config_path = f'configs/{config_name}.yaml'
+config = OmegaConf.load(config_path)
+config
+
+
+# Cargo el autoencoder ya entrenado:
+
+# In[3]:
+
+
+# 1. Crear una instancia del modelo (debe tener la misma arquitectura)
+autoencoder_cargado = ConfigurableAutoencoder(config=config)
+# 2. Carga los parámetros
+autoencoder_cargado.load_state_dict(torch.load(f'data/trained_models/{config_name}.pth'))
+# 3. Modo evaluación (cuando lo use para inferencia)
+autoencoder_cargado.eval()
+
+
+# Genero dataset de testeo:
+
+# In[9]:
+
+
+n = config['testing']['n']
+batch_size = config['testing']['batch_size']
+n_cuad_lado = config['training']['n_cuad_lado']
+pixeles_cuad = config['training']['pixeles_cuad']
+
+test_g, test_gi, test_gI0 = mixed_dataset(
+    n_total = n,
+    generate_multiple_images = generate_multiple_images,
+    conjunto_n_cuad_lado = n_cuad_lado,
+    conjunto_pixeles_cuad = pixeles_cuad,
+    ratios = config.training.get('ratio',[1]),
+)
 
