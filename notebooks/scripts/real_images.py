1943be8b1f793d941a1b97869c726ed1ada2d1cc
biancabalzarini <balzarini.bianca@gmail.com>
2025-04-28 20:56:28 -0300

    cargo pyread de otra forma para que no crashee - cargo autoencoder ya entrenado

--- notebooks/scripts/real_images.py
+++ notebooks/scripts/real_images.py
@@ -4,14 +4,32 @@
 # In[1]:
 
 
+import sys
+sys.path.append('..')
+
 import pyreadr # Para leer datos de tipo RData
 import numpy as np
 import cv2 as cv2
 import matplotlib.pyplot as plt
 from matplotlib import colors
 
+from omegaconf import OmegaConf
+from scripts.autoencoders import ConfigurableAutoencoder
+import torch
+
 
 # In[2]:
+
+
+try:
+    OmegaConf.register_new_resolver("eval", eval)
+except ValueError:
+    pass
+
+
+# Cargo imágenes SAR reales para visualizarlas
+
+# In[3]:
 
 
 def Ecualizauint16(img):
@@ -34,7 +52,7 @@ def plot_ecualized_image(img, title):
     plt.imshow(img4,cmap='gray'),plt.title(title), plt.axis('off')
 
 
-# In[3]:
+# In[4]:
 
 
 imageMunich = pyreadr.read_r('data/real_SAR_images/Munich.RData')
@@ -42,11 +60,36 @@ Munich = np.array(list(imageMunich.items())[0][1])
 plot_ecualized_image(Munich, 'Imagen de Munich')
 
 
-# In[4]:
+# In[5]:
 
 
 imageSanFran = pyreadr.read_r('data/real_SAR_images/AirSAR_SanFrancisc_Enxu.RData')
 SanFran = np.array(list(imageSanFran.items())[0][1])
 SanFranIm = SanFran[:,:,0] # Esta es la banda HH
 plot_ecualized_image(SanFranIm, 'Imagen de San Francisco, California, USA HH')
+
+
+# Elegir el archivo de configuración correspondiente:
+
+# In[6]:
+
+
+config_name = 'config_1' # Elegir
+
+config_path = f'configs/{config_name}.yaml'
+config = OmegaConf.load(config_path)
+config
+
+
+# Cargo el autoencoder ya entrenado
+
+# In[7]:
+
+
+# 1. Crear una instancia del modelo (debe tener la misma arquitectura)
+autoencoder_cargado = ConfigurableAutoencoder(config=config)
+# 2. Carga los parámetros
+autoencoder_cargado.load_state_dict(torch.load(f'data/trained_models/{config_name}.pth'))
+# 3. Modo evaluación (cuando lo use para inferencia)
+autoencoder_cargado.eval()
 
