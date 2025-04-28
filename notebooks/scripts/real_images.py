b65fb481e8948690bf10a4b53adae8114b032de6
biancabalzarini <balzarini.bianca@gmail.com>
2025-04-28 19:16:50 -0300

    armo funciones

--- notebooks/scripts/real_images.py
+++ notebooks/scripts/real_images.py
@@ -14,40 +14,39 @@ from matplotlib import colors
 # In[2]:
 
 
-imageMunich = pyreadr.read_r('data/real_SAR_images/Munich.RData')
-Munich = np.array(list(imageMunich.items())[0][1])
-
-img1 = np.array(Munich, dtype=np.float64)
-# Normalizar:
-img2 = (img1 - np.min(img1)) * 65536/(np.max(img1) - np.min(img1))
-# Pasar a entero
-img3 = np.array(img2, dtype=np.uint16)
-
 def Ecualizauint16(img):
-    hist,bins = np.histogram(img.flatten(),65536,[0,65536])
+    hist, bins = np.histogram(img.flatten(),65536,[0,65536])
     cdf = hist.cumsum()
     cdf_m = np.ma.masked_equal(cdf, 0)  
     cdf_m = (cdf_m - cdf_m.min())*65535/(cdf_m.max()-cdf_m.min())
     cdf_equ= np.ma.filled(cdf_m,0).astype('uint16')
     return cdf_equ[img]
 
-img4 = Ecualizauint16(img3)
-plt.imshow(img4,cmap='gray'),plt.title('Imagen de Munich'), plt.axis('off')
+def plot_ecualized_image(img, title):
+    img1 = np.array(img, dtype=np.float64)
+    # Normalizar:
+    img2 = (img1 - np.min(img1)) * 65536/(np.max(img1) - np.min(img1))
+    # Pasar a entero:
+    img3 = np.array(img2, dtype=np.uint16)
+    # Ecualizar:
+    img4 = Ecualizauint16(img3)
+    
+    plt.imshow(img4,cmap='gray'),plt.title(title), plt.axis('off')
 
 
 # In[3]:
 
 
+imageMunich = pyreadr.read_r('data/real_SAR_images/Munich.RData')
+Munich = np.array(list(imageMunich.items())[0][1])
+plot_ecualized_image(Munich, 'Imagen de Munich')
+
+
+# In[4]:
+
+
 imageSanFran = pyreadr.read_r('data/real_SAR_images/AirSAR_SanFrancisc_Enxu.RData')
 SanFran = np.array(list(imageSanFran.items())[0][1])
-
-img1 = np.array(SanFran, dtype=np.float64) # Tiene dimensión 9
-img2 = img1[:,:,0] # Esta es la banda HH
-# Normalizar:
-img3 = (img2 - np.min(img1)) * 65536/(np.max(img1) - np.min(img1))
-# Pasar a entero
-img4 = np.array(img3, dtype=np.uint16)
-
-img5 = Ecualizauint16(img4)
-plt.imshow(img5,cmap='gray'),plt.title('Imagen de San Francisco, California, USA HH'), plt.axis('off')
+SanFranIm = SanFran[:,:,0] # Esta es la banda HH
+plot_ecualized_image(SanFranIm, 'Imagen de San Francisco, California, USA HH')
 
