1f4f1d491863a8434071d95ac89db74eac303f77
biancabalzarini <balzarini.bianca@gmail.com>
2025-05-07 20:34:37 -0300

    corro quality filters con otro config

--- notebooks/scripts/quality_filters.py
+++ notebooks/scripts/quality_filters.py
@@ -37,7 +37,7 @@ except ValueError:
 # In[3]:
 
 
-config_name = 'config_simetrico-3conv_256encod' # Elegir
+config_name = 'config_simetrico-lr0_001' # Elegir
 
 config_path = f'configs/{config_name}.yaml'
 config = OmegaConf.load(config_path)
